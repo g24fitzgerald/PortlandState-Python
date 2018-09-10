73fc6c949025dcb6c9187ee0da0dbebb236dc184
Gina Fitzgerald <g24fitzgerald@gmail.com>
2018-09-10 09:20:34 -0500

    added get_answer and up_to_vowels functions

--- coursera/w3w4/loops_over_str.py
+++ coursera/w3w4/loops_over_str.py
@@ -16,7 +16,7 @@ def collect_vowels(s):
         if char in 'aeiouAEIOU':
             vowels = vowels + char
     return vowels
-    
+
 def count_vowels(s):
     '''(str) -> int
 
@@ -33,3 +33,40 @@ def count_vowels(s):
         if char in 'aeiouAEIOU':
             num_vowels = num_vowels + 1
     return num_vowels
+
+def up_to_vowels(s):
+    ''' (str) -> str
+
+    Return a substring of s from index 0 up to but
+    not including the first vowel in s
+
+    >>> up_to_vowels('hello')
+    'h'
+    >>> up_to_vowels('bcvir')
+    'bcvir'
+    >>> up_to_vowels('FYH')
+    'FYH'
+    '''
+
+    new_str = ''
+    i = 0
+    while i < len(s) and not (s[i] in 'aeiouAEIOU'):
+        new_str = new_str + s[i]
+        i = i + 1
+    return new_str
+
+def get_answer(prompt):
+    ''' (str) -> str
+
+    Use prompt to ask user for a 'yes' or 'no'
+    answer and continue asking until the user gives
+    a valid response. Return the answer
+
+    >>> get_answer('yes')
+    'yes'
+    '''
+
+    answer = input(prompt)
+    while not (answer == 'yes' or answer == 'no'):
+        answer = input(prompt)
+    return answer
