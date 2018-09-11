92247892163eb0fe4f34fc577d8dba62f6d6ed2b
Gina Fitzgerald <g24fitzgerald@gmail.com>
2018-09-11 16:46:33 -0500

    a1 assignment functions

--- coursera/w5w6/a3.py
+++ coursera/w5w6/a3.py
@@ -21,8 +21,10 @@ def is_valid_word(wordlist, word):
 
     >>> is_valid_word(['ANT', 'BOX', 'SOB', 'TO'], 'TO')
     True
+    >>> is_valid_word(['ANT', 'BOX', 'SOB', 'TO'], 'BOB')
+    False
     """
-
+    return word in wordlist
 
 def make_str_from_row(board, row_index):
     """ (list of list of str, int) -> str
@@ -32,8 +34,11 @@ def make_str_from_row(board, row_index):
 
     >>> make_str_from_row([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 0)
     'ANTT'
+    >>> make_str_from_row([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 1)
+    'XSOB'
     """
-
+    str_from_row = ''.join(board[row_index])
+    return str_from_row
 
 def make_str_from_column(board, column_index):
     """ (list of list of str, int) -> str
@@ -43,8 +48,13 @@ def make_str_from_column(board, column_index):
 
     >>> make_str_from_column([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 1)
     'NS'
+    >>> make_str_from_column([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 3)
+    'TB'
     """
-
+    str_from_column = ''
+    for word in board:
+        str_from_column = str_from_column + word[column_index]
+    return str_from_column
 
 def board_contains_word_in_row(board, word):
     """ (list of list of str, str) -> bool
@@ -57,6 +67,8 @@ def board_contains_word_in_row(board, word):
 
     >>> board_contains_word_in_row([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'SOB')
     True
+    >>> board_contains_word_in_row([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'BOB')
+    False
     """
 
     for row_index in range(len(board)):
@@ -79,6 +91,12 @@ def board_contains_word_in_column(board, word):
     False
     """
 
+    for column_index in range(len(board[0])):
+        if word in make_str_from_column(board, column_index):
+            return True
+
+    return False
+
 
 def board_contains_word(board, word):
     """ (list of list of str, str) -> bool
@@ -89,8 +107,12 @@ def board_contains_word(board, word):
 
     >>> board_contains_word([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'ANT')
     True
+    >>> board_contains_word([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'TO')
+    True
+    >>> board_contains_word([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'NB')
+    False
     """
-
+    return board_contains_word_in_row(board, word) or board_contains_word_in_column(board, word)
 
 def word_score(word):
     """ (str) -> int
@@ -104,17 +126,32 @@ def word_score(word):
 
     >>> word_score('DRUDGERY')
     16
+    >>> word_score('DRUDGER')
+    14
+    >>> word_score('DR')
+    0
+    >>> word_score('DRUD')
+    4
     """
+    points = len(word)
+    if points < 3:
+        points = 0
+    elif 7 <= points <= 9:
+        points = points * 2
+    elif 10 <= points:
+        points = points * 3
+    return points
 
 
 def update_score(player_info, word):
-    """ ([str, int] list, str) -> NoneType
+    """ ([str, int], str) -> NoneType
 
     player_info is a list with the player's name and score. Update player_info
     by adding the point value word earns to the player's score.
 
     >>> update_score(['Jonathan', 4], 'ANT')
     """
+    player_info[1] = player_info[1] + word_score(word)
 
 
 def num_words_on_board(board, words):
@@ -124,8 +161,14 @@ def num_words_on_board(board, words):
 
     >>> num_words_on_board([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], ['ANT', 'BOX', 'SOB', 'TO'])
     3
+    >>> num_words_on_board([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], ['ANT', 'BOB', 'NO', 'TO'])
+    2
     """
-
+    word_count = 0
+    for word in words:
+        if board_contains_word(board, word):
+            word_count = word_count + 1
+    return word_count
 
 def read_words(words_file):
     """ (file open for reading) -> list of str
@@ -136,7 +179,14 @@ def read_words(words_file):
     Precondition: Each line of the file contains a word in uppercase characters
     from the standard English alphabet.
     """
+    #Read the grades accumulating into list
+    words = [] #accumulator list
 
+    line = words_file.readline()
+    while line != '': #signifies that we haven't reached the end of the file
+        words.append(line.rstrip('\n'))
+        line = words_file.readline() #string of info for word
+    return words
 
 def read_board(board_file):
     """ (file open for reading) -> list of list of str
@@ -144,3 +194,10 @@ def read_board(board_file):
     Return a board read from open file board_file. The board file will contain
     one row of the board per line. Newlines are not included in the board.
     """
+    board = []
+
+    line = board_file.readline()
+    while line != '':
+        board.append(list(line.rstrip('\n')))
+        line = board_file.readline()
+    return board
